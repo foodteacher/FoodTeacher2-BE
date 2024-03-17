70f132bcda1649e0331bc3bc94d2973adf3b0bc3
ae-GYEONGWON <tjsxor50@gmail.com>
2024-03-17 21:31:28 +0900

    feat: alembic 적용
    기본 셋팅
    resolved: #1 기본 셋팅

--- FT_api/models/user_info.py
+++ FT_api/models/user_info.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from FT_api.db.session import Base
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     id: Mapped[int] = mapped_column(primary_key=True, nullable=False, autoincrement=True)
     kakao_id: Mapped[str] = mapped_column(String(255))
     name: Mapped[str] = mapped_column(String(255), nullable=True)
@@ -18,39 +18,3 @@ class User(Base):
     kakao_access_token: Mapped[str] = mapped_column(String(255))
     kakao_refresh_token: Mapped[str] = mapped_column(String(255))
     jwt_refresh_token: Mapped[str] = mapped_column(String(255))
-    user_diet_plan_info: Mapped["UserDietPlanInfo"] = relationship(back_populates="user")
-
-
-class UserDietPlanInfo(Base):
-    __tablename__ = 'user_diet_plan_info'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, comment='Auto Increment')
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), comment='Auto Increment')
-    created_at: Mapped[datetime] = mapped_column(nullable=True)
-    advice: Mapped[str] = mapped_column(String(255), nullable=True)
-    recommanded_exercise: Mapped[str] = mapped_column(String(255), nullable=True)
-    excess_calories: Mapped[float] = mapped_column(nullable=True)
-    user: Mapped["User"] = relationship(back_populates="user_diet_plan_info")
-    menus: Mapped[List["Menu"]] = relationship(back_populates="user_diet_plan_info")
-    exercise: Mapped[List["Exercise"]] = relationship(back_populates="user_diet_plan_info")
-
-
-class Menu(Base):
-    __tablename__ = 'menu'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, comment='Auto Increment')
-    user_diet_plan_info_id: Mapped[int] = mapped_column(ForeignKey('user_diet_plan_info.id'), comment='Auto Increment')
-    name: Mapped[str] = mapped_column(String(255), nullable=True)
-    calories: Mapped[float] = mapped_column(nullable=True)
-    car: Mapped[float] = mapped_column(nullable=True)
-    pro: Mapped[float] = mapped_column(nullable=True)
-    fat: Mapped[float] = mapped_column(nullable=True)
-    meal_time: Mapped[str] = mapped_column(String(255), nullable=True)
-    user_diet_plan_info: Mapped["UserDietPlanInfo"] = relationship(back_populates="menus")
-
-
-class Exercise(Base):
-    __tablename__ = 'exercise'
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, comment='Auto Increment')
-    user_diet_plan_info_id: Mapped[int] = mapped_column(ForeignKey('user_diet_plan_info.id'), comment='Auto Increment')
-    my_exercise: Mapped[str] = mapped_column(String(255), nullable=True)
-    my_exercise_calories: Mapped[float] = mapped_column(nullable=True)
-    user_diet_plan_info: Mapped["UserDietPlanInfo"] = relationship(back_populates="exercise")
---
+++ server.py
@@ -0,0 +1,4 @@
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("FT_api.main:FT_api", host="localhost", port=8000, reload=True, proxy_headers=True)
