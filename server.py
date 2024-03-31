7f79d80a7e05e4bc44ef142b296deb4c422ddf73
ae-GYEONGWON <tjsxor50@gmail.com>
2024-03-31 14:46:36 +0900

    misc

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import uvicorn
 
-# if __name__ == "__main__":
-#     uvicorn.run("FT_api.main:FT_api", host="0.0.0.0", port=8000, reload=True, proxy_headers=True)
-
 if __name__ == "__main__":
-    uvicorn.run("FT_api.main:FT_api", host="localhost", port=8000, reload=True, proxy_headers=True)
+    uvicorn.run("FT_api.main:FT_api", host="0.0.0.0", port=8000, reload=True, proxy_headers=True)
+
+# if __name__ == "__main__":
+#     uvicorn.run("FT_api.main:FT_api", host="localhost", port=8000, reload=True, proxy_headers=True)
