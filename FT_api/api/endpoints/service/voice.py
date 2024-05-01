5f68c6867fbfd8ca6fce320b20d110ffcad33940
ae-GYEONGWON <tjsxor50@gmail.com>
2024-05-01 17:49:36 +0900

    modify: tts 코드 수정

--- FT_api/api/endpoints/service/voice.py
+++ FT_api/api/endpoints/service/voice.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import StreamingResponse
 from FT_api.core.config import get_setting
+from FT_api.schemas.voice import TTSRequest
 
 import urllib.request
 import urllib.parse
@@ -9,14 +10,14 @@ router = APIRouter()
 settings = get_setting()
 
 
-@router.get("/tts")
-def text_to_speech(text: str):
+@router.post("/tts")
+def text_to_speech(request: TTSRequest):
     speaker: str = "nara"
     volume: int = 0
     speed: int = 0
     pitch: int = 0
     url = "https://naveropenapi.apigw.ntruss.com/tts-premium/v1/tts"
-    data = f"speaker={speaker}&volume={volume}&speed={speed}&pitch={pitch}&format=mp3&text={urllib.parse.quote(text)}"
+    data = f"speaker={speaker}&volume={volume}&speed={speed}&pitch={pitch}&format=mp3&text={urllib.parse.quote(request.text)}"
 
     request = urllib.request.Request(url)
     request.add_header("X-NCP-APIGW-API-KEY-ID", settings.NCP_CLIENT_ID)
@@ -24,7 +25,13 @@ def text_to_speech(text: str):
 
     try:
         with urllib.request.urlopen(request, data=data.encode("utf-8")) as response:
-            if response.getcode() == 200:
-                return StreamingResponse(response, media_type="audio/mpeg")
+            if response.status == 200:
+                def stream_generator():
+                    while True:
+                        chunk = response.read(4096)  # 4096 바이트씩 읽기
+                        if not chunk:
+                            break
+                        yield chunk
+                return StreamingResponse(stream_generator(), media_type="audio/mpeg")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
