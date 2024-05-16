5cc16a2bf266e7dbd4024ee7130c35dff3d13260
ae-GYEONGWON <tjsxor50@gmail.com>
2024-05-17 03:20:20 +0900

    misc

--- FT_api/crud/user.py
+++ FT_api/crud/user.py
@@ -9,7 +9,7 @@ from FT_api.schemas.user import UserCreate, UserUpdate
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_social_id(self, db: Session, *, social_id: str) -> Optional[User]:
-        return db.query(User).filter(User.user_id == social_id).first()
+        return db.query(User).filter(User.user_social_id == social_id).first()
 
     def create(self, db: Session, *, obj_in: UserCreate) -> User:
         db_obj = User(**obj_in.model_dump(exclude_unset=True))
